ac3ea5f43bc8312013da313e537502c3cf7aa897
Emmanuel-Guerreiro <64450981+Emmanuel-Guerreiro@users.noreply.github.com>
2020-05-27 22:07:45 -0300

    Agregado notificación de agua y caminar

--- recordatorio 1.1/main.py
+++ recordatorio 1.1/main.py
@@ -3,26 +3,51 @@ from datetime import datetime, timedelta
 from time import sleep
 
 tiempo_inicio = datetime.now()
-masVeinteMinutos = tiempo_inicio + timedelta(minutes = 20)
+mas_veinte_minutos = tiempo_inicio + timedelta(minutes = 20)
+mas_cuarenta_minutos = tiempo_inicio + timedelta(minutes = 40)
+mas_sesenta_minutos = tiempo_inicio + timedelta(minutes= 60)
 
-def notificacion():
-    toaster = ToastNotifier()
-    toaster.show_toast("Cuidado de ojos", "Aflojar la vista durante 20 segundos", icon_path = "ojo.ico", duration = 20)
+
+class notificaciones:
+    
+    def __init__(self, nombre, mensaje, icono, duracion):
+        self.nombre = nombre
+        self.mensaje = mensaje
+        self.icono = icono
+        self.duracion = duracion
+        
+    
+    def mostrar_notificacion(self):
+        toaster = ToastNotifier()
+        toaster.show_toast(self.nombre, self.mensaje, icon_path = self.icono, duration = self.duracion)
+
+
+notificacion_ojos = notificaciones('Cuidado de ojos', 'Descansa la vista durante 20 segundos', 'ojo.ico', 7)
+notificacion_movimiento = notificaciones('Moverse', 'Levantate y mueve un poco las piernas', 'ojo.ico', 7)
+notificacion_agua = notificaciones('Tomar agua', 'Toma agua para mantenerte hidratado', 'ojo.ico', 7)
+
+
 
 def corroborar_hora():
 
-    global tiempo_inicio, masVeinteMinutos
+    global tiempo_inicio, mas_veinte_minutos, mas_sesenta_minutos, mas_cuarenta_minutos
 
     while True:
-        if tiempo_inicio >= masVeinteMinutos:
-            notificacion()
+        if tiempo_inicio >= mas_veinte_minutos:
+            notificacion_ojos.mostrar_notificacion()
             tiempo_inicio = datetime.now()
-            masVeinteMinutos = tiempo_inicio + timedelta(minutes = 20)
+            mas_veinte_minutos = tiempo_inicio + timedelta(seconds = 20)
+        elif tiempo_inicio >= mas_sesenta_minutos:
+            notificacion_movimiento.mostrar_notificacion()
+            tiempo_inicio = datetime.now()
+            mas_sesenta_minutos = tiempo_inicio + timedelta(seconds = 20)
+        elif tiempo_inicio >= mas_cuarenta_minutos:
+            notificacion_agua.mostrar_notificacion()
+            tiempo_inicio = datetime.now()
+            mas_cuarenta_minutos = tiempo_inicio + timedelta(seconds = 20)
         else:
-            sleep(600)
+            sleep(10)
             tiempo_inicio = datetime.now()
             corroborar_hora()
 
 corroborar_hora()
-
-notificacion()
